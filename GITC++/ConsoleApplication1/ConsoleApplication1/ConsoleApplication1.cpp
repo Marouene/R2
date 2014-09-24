31a18572481b52849bbc25155d252b8bfb7dddb7
Marouene <marouene1983@yahoo.fr>
2014-09-24 16:23:11 +0200

    GGG

--- GITC++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ GITC++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,7 +10,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout << "rrrrrrrrr\n";
 	cout << "oui c'DDDDestJJHHN?JBJJJJJJJFRFEFEFEFE8888888899999111111111FEF moi le roodddd\n";
 	cout << "ça doit étre boHHHHHHSZXZZZXZXZZXZZZZXZn....\n";
-	cout << "dernier deSS2SDVDVDVVDS22veloppement\n";
+	cout << "dernier deS  CCCCCCCS2SDVDVDVVDS22veloppement\n";
 
 	system("pause");
 	return 0;
