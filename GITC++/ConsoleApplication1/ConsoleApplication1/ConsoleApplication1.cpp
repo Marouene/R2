8cc9bc4e2fcbf82dafdc6672467b584ed38b8c29
Marouene <marouene1983@yahoo.fr>
2014-09-24 15:58:12 +0200

    VERSION 2

--- GITC++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ GITC++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "rrrrrrrrr\n";
+	cout << "oui c'DDDDestJJHHN?JBJJJJJJJFRFEFEFEFEFEF moi le roodddd\n";
 	system("pause");
 	return 0;
 }
